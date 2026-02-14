47220cc4906b36326730a2af5cb3e67fe241249a
Chidi-A <chidiace93@gmail.com>
2026-02-14 07:25:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Scrimba React Challenges (Vite + React)
+This project is a single Vite + React app that contains multiple Scrimba React challenges in one codebase.
+
+Tech stack
+React + Vite.
+​
+
+JavaScript (ESM).
+
+CSS for styling.
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ChallengeVideoGameButton from './challenges/video-game-button/index.jsx';
+import ChallengeReactStudyBuddy from './challenges/react-study-buddy/index.jsx';
+import ChallengeRpgCharacterCreator from './challenges/rpg-character-creator/index.jsx';
+import ChallengeWorldMostAnnoyingForm from './challenges/world-most-annoying-form/index.jsx';
+import ChallengeTheLegendOfScrimba from './challenges/the-legend-of-scrimba/index.jsx';
+
+function App() {
+  return (
+    <div>
+      <>
+        <ChallengeTheLegendOfScrimba />
+      </>
+    </div>
+  );
+}
+
+export default App;
